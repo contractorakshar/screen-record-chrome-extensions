19fbdb8de4e4673e07200d4be12e404ce5713964
contractorakshar <contractorakshar@gmail.com>
2021-08-11 16:29:50 +0530

    ⚡️ manifest v2 chrome ext screen recording

---
+++ common.js
@@ -0,0 +1,46 @@
+let recorder, stream;
+const startbtn = document.getElementById('start');
+const stopbtn = document.getElementById('stop');
+const pause = document.getElementById('pause');
+const resume = document.getElementById('resume');
+const video = document.querySelector('video');
+
+const startRecording = async () => {
+  stream = await navigator.mediaDevices.getDisplayMedia({
+    video: { mediaSource: 'screen' },
+  });
+  recorder = new MediaRecorder(stream);
+
+  const chunks = [];
+  recorder.ondataavailable = (e) => chunks.push(e.data);
+
+  recorder.onstop = (e) => {
+    const completeBlob = new Blob(chunks, { type: chunks[0].type });
+    video.src = URL.createObjectURL(completeBlob);
+  };
+
+  recorder.start(200);
+};
+
+stopbtn.addEventListener('click', () => {
+  stopbtn.setAttribute('disabled', true);
+  startbtn.removeAttribute('disabled');
+  if (recorder.state !== 'inactive') {
+    recorder.stop();
+  }
+  err.style.display = 'none';
+});
+
+pause.addEventListener('click', () => {
+  recorder.pause();
+});
+
+resume.addEventListener('click', () => {
+  recorder.resume();
+});
+
+startbtn.addEventListener('click', function () {
+  start.setAttribute('disabled', true);
+  stopbtn.removeAttribute('disabled');
+  startRecording();
+});
